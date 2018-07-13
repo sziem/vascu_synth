e5c88d4762634173af937e99d847f87bf3ab07a3
Sönke Ziemer <soezie@users.noreply.github.com>
2018-07-13 18:24:18 +0200

    fixed bug where data loaded was not sorted.  Added function to bundle pngs to hdf5-file.

--- read_vascular_structures.py
+++ read_vascular_structures.py
@@ -11,12 +11,14 @@ import imageio
 import glob
 from os import path
 import toolbox.view3d as view3d
+import h5py
 
 def read_vascular_volume(impath):
     ims = list()
     """read all png-files in impath and return them as np-array.
        Shape will be in the order (z,y,x), if files in impath were (x,y,z) """
-    for file_name in glob.glob(path.join(impath, "*.png")):
+    for file_name in sorted(glob.glob(path.join(impath, "*.png"))):
+        #print(file_name)
         ims.append(imageio.imread(file_name))
     return np.array(ims)
 
@@ -26,9 +28,16 @@ def display_vascular_volume(impath):
     print("max value: ", np.max(im))
     view3d.quick_max_projection_viewer(im, z_axis=0)
 
+def bundle_h5(impath, filename="file.hdf5"):
+    """for export to e.g. fiji"""
+    im = read_vascular_volume(impath)
+    with h5py.File(filename, 'w') as data_file:
+        data_file.create_dataset('image_stack', data=im)
+
 def main():
-    path_to_images = path.join("image1", "original_image")
+    path_to_images = path.join("image0", "original_image")
     display_vascular_volume(path_to_images)
+#    bundle_h5(path_to_images, filename="image0.hdf5")
 
 if __name__ == '__main__':
     main()
