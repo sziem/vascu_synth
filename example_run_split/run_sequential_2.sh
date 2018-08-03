16ede4de67882bfa787bab26b3342829b4b39c6b
Sönke Ziemer <soezie@users.noreply.github.com>
2018-08-03 15:34:06 +0200

    example which was used to generate seed6

---
+++ example_run_split/run_sequential_2.sh
@@ -0,0 +1,4 @@
+./VascuSynthPng image_params_split50.txt image_names_split50.txt 0.03; 
+./VascuSynthPng image_params_split60.txt image_names_split60.txt 0.03; 
+./VascuSynthPng image_params_split70.txt image_names_split70.txt 0.03; 
+./VascuSynthPng image_params_split80.txt image_names_split80.txt 0.03;
