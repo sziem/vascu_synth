2cf747936886837c2ebf644f3d7716ca4e0f4a9a
Sönke Ziemer <soezie@users.noreply.github.com>
2018-08-24 12:03:05 +0200

    generate_dataset.sh writes seed to file now.

--- generate_dataset.sh
+++ generate_dataset.sh
@@ -1,6 +1,8 @@
 # run this to generate a dataset in one step.
 
-echo "usage: ./generate_dataset SEED N_IMAGES(200 by default)"
+echo "usage: ./generate_dataset SEED N_IMAGES(200 by default)" &&
+echo "seed"$1;
+echo "seed"$1>seed.txt &&
 python generate_vascu_config.py $1 $2 &&
 ./split_and_run.sh $1
 
